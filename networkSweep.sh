9d3b7dfae3f7bd7bb8f08a687fb0f567204359c8
Lach <51766354+lachlanellison@users.noreply.github.com>
2019-11-06 06:16:11 +1000

    Create networkSweep.sh

---
+++ networkSweep.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+Sweep your currently connected Network, script will automatically detect your Ip/ / CIDR. If needed you can modify the script t oscan an alternative Network.
+x=$(ifconfig | grep "inet " | grep -v 127.0.0.1 | cut -d"." -f1-3 | tr -d "inet"|tr -d " ") 
+for y in `seq 1 254`; do 
+ping -c 1 $x.$y | grep "64 bytes" | cut -d " " -f 4 | tr -d ':' &
+done
