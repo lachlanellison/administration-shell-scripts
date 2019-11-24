7b4254fa304b30c6e61831bb1f953d8a0af0c219
Lach <51766354+lachlanellison@users.noreply.github.com>
2019-11-24 20:41:07 +1000

    Update cpu-usage.sh

--- cpu-usage.sh
+++ cpu-usage.sh
@@ -3,7 +3,7 @@ while [ true ] ;do
 used=`free -m |awk 'NR==3 {print $4}'`
 
 if [ $used -lt 1000 ] && [ $used -gt 800 ]; then
-echo "Free memory is below 1000MB. Check running processes, something is eating you CPU!" | /bin/mail -s "HIGH MEMORY ALERT!" user@mydomain.com
+echo "Free memory is below 1000MB. Check running processes, something is eating your CPU!" | /bin/mail -s "HIGH MEMORY ALERT!" user@mydomain.com
 
 
 fi
