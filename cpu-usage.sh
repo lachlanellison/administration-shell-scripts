6886811650948bdaf64d331a563921aa963cf63e
Lach <51766354+lachlanellison@users.noreply.github.com>
2019-11-24 20:38:44 +1000

    Add files via upload

---
+++ cpu-usage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while [ true ] ;do
+used=`free -m |awk 'NR==3 {print $4}'`
+
+if [ $used -lt 1000 ] && [ $used -gt 800 ]; then
+echo "Free memory is below 1000MB. Check running processes, something is eating you CPU!" | /bin/mail -s "HIGH MEMORY ALERT!" user@mydomain.com
+
+
+fi
+sleep 5
+done
+
